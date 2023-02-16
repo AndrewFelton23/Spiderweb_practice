ee39561df51984ebcf234ce03be206558d119875
AndrewFelton23 <108468948+AndrewFelton23@users.noreply.github.com>
2023-02-16 13:37:25 +0200

    added files to the flask folder

---
+++ Flask/imageDisplay.py
@@ -0,0 +1,24 @@
+from flask import Flask, Response
+import cv2
+
+
+app = Flask(__name__)
+
+@app.route('/img/')
+def img_display():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace;boundary=frame')
+def gen():
+    #process the image
+    img = cv2.imread("/Users/andrewfelton/Downloads/Spiderweb Labs/Flask/resources/test_image.jpg")
+    #encode the image as a jpeg file
+    _,enc_img = cv2.imencode('.jpg',img)
+    #convert the coded image to bytes
+    frame_img = enc_img.tobytes()
+    #return the coded image as a flask response
+    yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame_img +b'\r\n\r\n')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=8080)
