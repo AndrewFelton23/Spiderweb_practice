1a49f7c8e0dd6160f1c2936e74829237a3dffcba
AndrewFelton23 <108468948+AndrewFelton23@users.noreply.github.com>
2023-02-16 15:16:39 +0200

    yolov8 stuff

--- YoloV8_practice/Image_Detection.py
+++ YoloV8_practice/Image_Detection.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 #instantiate the model object
 model = YOLO("yolov8n.pt")
 
-#Streaming from a webcam
-results = model.predict(source="",save=True, show=True)  # save predictions as labels
+#detect on a image and save the results
+results = model.predict(source="https://github.com/AndrewFelton23/Spiderweb_practice/blob/39d3fa14de106049d1c3f3eb8b40c8f4dfd9445e/YoloV8_practice/resource/test_image.jpg",save=True, show=True)  # save predictions as labels
 boxes = results[0].boxes
 for detected in boxes.cls:
     if detected == 0:
